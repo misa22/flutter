a31dd250968015fe784ca03295aafc04c9a07154
misa22 <misael.solano.m@gmail.com>
2020-02-11 09:02:57 -0600

    agregados de input

---
+++ lib/cuerpo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:login/login.dart';
+
+class Cuerpo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return new Scaffold(
+        body: new Stack(
+      children: <Widget>[
+        new ColorBack(),
+        new Container(
+          margin: EdgeInsets.fromLTRB(0, 500, 0, 20),
+          width: size.width,
+          height: 150,
+          decoration: BoxDecoration(color: Colors.white),
+          alignment: Alignment.center,
+          child: new Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Ingresa tu usuario',
+                    hoverColor: Colors.black
+              ),),
+              TextField(
+                decoration: InputDecoration(labelText: 'Ingresa tu contraseña'),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -25,4 +25,4 @@ class ColorBack extends StatelessWidget{
       ),
     ) ;
   }
-}
+} 
